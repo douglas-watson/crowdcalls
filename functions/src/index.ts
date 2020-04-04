c6bcfe7443906e339e54031bbd92ee12d24d5df7
Andrew Watson <andy@watsons.ch>
2020-04-04 16:36:08 +0200

    add example of firestore phone number retrieval

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,8 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp(functions.config().firebase);
 
+const db = admin.firestore();
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -26,3 +28,34 @@ export const apiTest = functions.https.onRequest((request, response) => {
   const apiKey = functions.config().twilio.key
   response.send(`My API key is ${apiKey}.`);
 });
+
+
+// const getClosestNumber = function(postcode: number) {
+//   return postcode;
+// } 
+
+export const retrieveNumber = functions.https.onRequest((request, response) => {
+  const volunteersRef = db.collection('volunteers');
+  volunteersRef.orderBy('postcode').limit(10).get()
+  .then(snapshot => {
+    if (snapshot.empty) {
+      console.log("No entries found");
+      response.sendStatus(404);
+      return;
+    }
+
+    let results = "";
+    snapshot.forEach(volunteer => {
+      const data = volunteer.data();
+      console.log(data.postcode, '->', data.phone);
+      results += `Postcode: ${data.postcode} -> ${data.phone}\n`;
+    })
+
+    response.set('Content-Type', 'text/plain');
+    response.send(results);
+  })
+  .catch(err => {
+    console.log("Error getting volunteers", err);
+    response.sendStatus(500);
+  })
+})
