5475f17165c4b52495f9c4a8bdb6979ff6f7693d
Douglas Watson <douglas@watsons.ch>
2020-04-04 22:30:40 +0200

    first working calling chain w/ hard-coded numbers

--- README.md
+++ README.md
@@ -4,6 +4,32 @@ Crowdsourcing hotlines
 
 ## Getting started
 
+Install node and npm, then install firebase tools:
+
+    npm install -g firebase-tools
+
+Authorize the firebase CLI:
+
+    firebase login
+
+Clone this repo
+
+    git clone git@github.com:douglas-watson/crowdcalls.git
+    cd crowdcalls/
+
+Associate the local files with the remote firebase project:
+
+    firebase use --add
+
+Give the project any name, for example default.
+
+Install npm dependencies
+
+    cd functions/
+    npm install
+
+## Admin
+
 Set up your account by adding your API key to firebase:
 
 ```
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,15 +13,42 @@ const db = admin.firestore();
 //  response.send("Hello from Firebase!");
 // });
 
+const urlForCallVolunteer = function (numbers: string[]) {
+  return "/callVolunteer/" + numbers.join('/')
+};
+
 export const welcomeCall = functions.https.onRequest(async (request, response) => {
-    const voiceResponse = new twilio.twiml.VoiceResponse();
-    voiceResponse.say("Welcome to crowd calls.");
-    voiceResponse.pause({ length: 1 });
-    voiceResponse.say("The future is now.");
-    voiceResponse.pause({ length: 1 });
-    voiceResponse.say({ voice: 'alice', language: 'fr-CA' }, 'Bienvenue. Le future est radieux.');
-    voiceResponse.stop();
-    return response.contentType('text/xml').send(voiceResponse.toString());
+  const selectedNumbers = ['+41774165457', '+41774865315', '+41774165457']
+  const nextNumber = selectedNumbers.shift()
+  const voiceResponse = new twilio.twiml.VoiceResponse()
+  voiceResponse.say({ voice: 'alice', language: 'fr-CA' }, 'Bienvenue. Un volontaire va recevoir votre appel.')
+  voiceResponse.dial({
+    action: urlForCallVolunteer(selectedNumbers),
+  }, nextNumber)
+  return response.contentType('text/xml').send(voiceResponse.toString())
+});
+
+export const callVolunteer = functions.https.onRequest(async (request, response) => {
+  // Expects a path in the format /number1/number2/number3
+  const voiceResponse = new twilio.twiml.VoiceResponse()
+
+  let selectedNumbers = request.path.split("/").filter(x => x.length > 0)
+  if (!selectedNumbers || selectedNumbers.length == 0) {
+    console.log("No numbers to call, end of the line.")
+    voiceResponse.say("Aucun volontaire n'est disponible pour le moment.")
+    return response.contentType('text/xml').send(voiceResponse.toString())
+  }
+
+  // TODO check if call went through. If it did, finish.
+  voiceResponse.say("Cette personne n'est pas disponible. Nous appelons un nouveau volontaire.")
+
+  let nextNumber = selectedNumbers.shift()
+  console.log("Calling " + nextNumber + " falling back to " + urlForCallVolunteer(selectedNumbers))
+  voiceResponse.dial({
+    action: urlForCallVolunteer(selectedNumbers),
+  }, nextNumber)
+
+  return response.contentType('text/xml').send(voiceResponse.toString())
 });
 
 export const apiTest = functions.https.onRequest((request, response) => {
@@ -37,25 +64,25 @@ export const apiTest = functions.https.onRequest((request, response) => {
 export const retrieveNumber = functions.https.onRequest((request, response) => {
   const volunteersRef = db.collection('volunteers');
   volunteersRef.orderBy('postcode').limit(10).get()
-  .then(snapshot => {
-    if (snapshot.empty) {
-      console.log("No entries found");
-      response.sendStatus(404);
-      return;
-    }
+    .then(snapshot => {
+      if (snapshot.empty) {
+        console.log("No entries found");
+        response.sendStatus(404);
+        return;
+      }
 
-    let results = "";
-    snapshot.forEach(volunteer => {
-      const data = volunteer.data();
-      console.log(data.postcode, '->', data.phone);
-      results += `Postcode: ${data.postcode} -> ${data.phone}\n`;
+      let results = "";
+      snapshot.forEach(volunteer => {
+        const data = volunteer.data();
+        console.log(data.postcode, '->', data.phone);
+        results += `Postcode: ${data.postcode} -> ${data.phone}\n`;
+      })
+
+      response.set('Content-Type', 'text/plain');
+      response.send(results);
     })
-
-    response.set('Content-Type', 'text/plain');
-    response.send(results);
-  })
-  .catch(err => {
-    console.log("Error getting volunteers", err);
-    response.sendStatus(500);
-  })
+    .catch(err => {
+      console.log("Error getting volunteers", err);
+      response.sendStatus(500);
+    })
 })
